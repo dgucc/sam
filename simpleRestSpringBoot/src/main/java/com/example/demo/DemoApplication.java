6c9267774e97ea74b6aafb01adce3d4b874cb338
dgucc <djvu.guchz@gmail.com>
2026-04-11 05:19:07 +0200

    move static web page + cleaned out  DemoApplication

--- simpleRestSpringBoot/src/main/java/com/example/demo/DemoApplication.java
+++ simpleRestSpringBoot/src/main/java/com/example/demo/DemoApplication.java
@@ -2,19 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-@RequestMapping("/")
 public class DemoApplication {
-
-    @GetMapping("/")
-    public String home(){
-        return "Hello World!<br>See <a href='http://localhost:8080/universities'>Universities</a>";
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
