af0e8712a542e14927474eb72ee88511c35c5168
dgucc <djvu.guchz@gmail.com>
2026-04-12 06:40:43 +0200

    deleteUniversityById()

--- simpleRestSpringBoot/src/main/java/com/example/demo/controller/UniversityController.java
+++ simpleRestSpringBoot/src/main/java/com/example/demo/controller/UniversityController.java
@@ -5,7 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,6 +38,13 @@ public class UniversityController {
         } catch (Exception e) {
             return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
+    }
+
+
+    @DeleteMapping("/universities/{id}")
+    public String deleteUniversityById(@PathVariable("id")Long id) {
+        service.deleteUniversity(id);
+        return "University [" + id + "] Deleted";
     }
 
 
--- simpleRestSpringBoot/src/main/java/com/example/demo/service/UniversityService.java
+++ simpleRestSpringBoot/src/main/java/com/example/demo/service/UniversityService.java
@@ -17,6 +17,10 @@ public class UniversityService {
         return repository.save(university);
     }
 
+    public void deleteUniversity(Long id){
+        repository.deleteById(id);
+    }
+
     public List<University> getListOfUniversities() {
         return repository.findAll();
     }
